ae2dcb209c9e3e78448420346a491854ecc99550
David Ivy <david@david-VirtualBox.(none)>
2012-10-21 22:12:23 -0400

    Initial Commit

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,40 @@
+class SearchController < ApplicationController
+  def index
+  	# receives search parameters and returns html for query #
+    validate_and_send_query(params[:search])
+
+    respond_to do |format|
+        format.html # show.html.erb
+        format.json
+        format.js
+    end
+  end
+
+  def preview
+  	# receives search parameters and returns ajax data for query #
+    validate_and_send_query(params[:search])
+    render :partial => 'tweets', :content_type => 'text/html'
+  end
+
+  def send_query(query, number)
+  	# sends query to twitter #
+    client = TwitterSearch::Client.new('tweetersearch')
+    @tweets =  client.query(:q => query, :rpp => number)
+  end
+
+  def valid(query)
+  	# validates input data, if blank #
+    if query.blank?
+        @error = 'Please provide input for search'
+        return false
+    end
+    return true
+  end
+
+  def validate_and_send_query(query)
+  	# sends twitter query if input is valid #
+    if valid(query)
+        send_query(query, '100')
+    end 
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApp::Application.config.secret_token = '3865253bf68b0e7574235a137c77dc78bafe26843388e62b972c0528cb04518a891834802119a345b82553eb6cac0fb72611632ab9785594025f12631f45de29'
