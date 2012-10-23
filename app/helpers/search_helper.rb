36771bf74400a7130c6f655bb5e0fc5ea2ba11d8
David Ivy <david@david-VirtualBox.(none)>
2012-10-23 18:53:59 -0400

    Added search_helper files

---
+++ app/helpers/search_helper.rb
@@ -0,0 +1,18 @@
+module SearchHelper
+  def send_query(query, max_id=nil, page=nil, number=10)
+  	# sends query to twitter #
+    params = { q: query, rpp: number }
+    params = params.merge( max_id: max_id ) if max_id
+    params = params.merge( page: page ) if page
+    
+    client = TwitterSearch::Client.new('tweetersearch')
+    @tweets =  client.query( params )
+    @max_id = max_id || @tweets.count > 0 ? @tweets.first.id : nil
+    @page = Integer(page || 1)
+  end
+
+  def valid(query)
+  	# validates input data, if blank #
+    ! query.blank?
+  end
+end
---
+++ spec/helpers/search_helper_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe SearchHelper do
+
+  describe 'valid' do
+    it 'empty query should return false' do
+      helper.valid('').should == false
+    end
+
+    it 'blank query should return false' do
+      helper.valid(' ').should == false
+    end
+
+    it 'real query should return true' do
+      helper.valid('huzzah ').should == true
+    end
+  end
+
+  describe 'send_query' do
+    it 'test query should return 30 results' do
+      helper.send_query('test').count.should == 30
+    end
+
+    it 'test query w/ supplied number should return correct results' do
+      helper.send_query('test', 100).count.should == 100
+    end
+
+    it 'randomized result should return 0' do
+      random_query = SecureRandom.hex(10)
+      helper.send_query(random_query).count.should == 0
+    end
+  end
+
+end
