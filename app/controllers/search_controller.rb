240dfafb4ccd7c3a96aa326e0215935f77ba5af5
David Ivy <david@david-VirtualBox.(none)>
2012-10-23 14:31:31 -0400

    Added link pagination, needs ajax and class change

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,10 @@ class SearchController < ApplicationController
   respond_to :html, :json
   def twitter
     # receives search parameters and returns html for query #
-    send_query(params[:search]) if valid(params[:search])
+    @query = params[:search]
+    send_query(@query, params[:max_id], params[:page]) if valid(@query)
+    @max_id = params[:max_id] || @tweets.first.id
+    @page = Integer(params[:page]) || 1
 
     respond_to do |format|
       format.html
